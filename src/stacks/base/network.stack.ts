79fdc81c8fbb71fd5395b76c68cc717e1919167b
burakbakirdev <iburakbakir@gmail.com>
2025-08-05 16:00:49 +0300

    refactor(eip and nat commented): eip and nat configurations are commented to avoid being billed

--- src/stacks/base/network.stack.ts
+++ src/stacks/base/network.stack.ts
@@ -2,15 +2,17 @@ import { Construct } from "constructs";
 import { BaseEnvironment } from "../../environments/base-environment";
 import { Vpc } from "../../../.gen/providers/huaweicloud/vpc/index";
 import { VpcSubnet } from "../../../.gen/providers/huaweicloud/vpc-subnet/index";
-import { NatGateway } from "../../../.gen/providers/huaweicloud/nat-gateway/index";
-import { NatSnatRule } from "../../../.gen/providers/huaweicloud/nat-snat-rule/index";
-import { VpcEip } from "../../../.gen/providers/huaweicloud/vpc-eip";
+// import { NatGateway } from "../../../.gen/providers/huaweicloud/nat-gateway/index";
+// import { NatSnatRule } from "../../../.gen/providers/huaweicloud/nat-snat-rule/index";
+// import { VpcEip } from "../../../.gen/providers/huaweicloud/vpc-eip";
+
+// Please remove all comment lines to deploy a application on CCE.
 
 export class NetworkStack extends Construct {
   private vpc: Vpc;
   private subnets = new Map<string, VpcSubnet>();
-  private nat: NatGateway;
-  private snat: NatSnatRule;
+  // private nat: NatGateway;
+  // private snat: NatSnatRule;
 
   constructor(scope: Construct, id: string, environment: BaseEnvironment) {
     super(scope, id);
@@ -38,31 +40,31 @@ export class NetworkStack extends Construct {
       number++;
     }
 
-    const eip = new VpcEip(this, `eip-cce`, {
-      publicip: {
-        type: "5_bgp",
-      },
-      bandwidth: {
-        shareType: "PER",
-        name: `nat`,
-        size: 300,
-        chargeMode: "traffic",
-      },
-    });
+    // const eip = new VpcEip(this, `eip-cce`, {
+    //   publicip: {
+    //     type: "5_bgp",
+    //   },
+    //   bandwidth: {
+    //     shareType: "PER",
+    //     name: `nat`,
+    //     size: 300,
+    //     chargeMode: "traffic",
+    //   },
+    // });
 
-    this.nat = new NatGateway(this, "nat", {
-      name: `nat-${environment.name}`,
-      spec: "1",
-      vpcId: this.vpc.id,
-      subnetId: this.getSubnetId(`snet-${environment.name}-cce`),
-    });
+    // this.nat = new NatGateway(this, "nat", {
+    //   name: `nat-${environment.name}`,
+    //   spec: "1",
+    //   vpcId: this.vpc.id,
+    //   subnetId: this.getSubnetId(`snet-${environment.name}-cce`),
+    // });
 
-    this.snat = new NatSnatRule(this, "snat", {
-      natGatewayId: this.nat.id,
-      floatingIpId: eip.id,
-      subnetId: this.getSubnetId(`snet-${environment.name}-cce`),
-      sourceType: 0,
-    });
+    // this.snat = new NatSnatRule(this, "snat", {
+    //   natGatewayId: this.nat.id,
+    //   floatingIpId: eip.id,
+    //   subnetId: this.getSubnetId(`snet-${environment.name}-cce`),
+    //   sourceType: 0,
+    // });
   }
 
   public get vpcId(): string {
@@ -83,11 +85,11 @@ export class NetworkStack extends Construct {
     throw new Error(`Invalid subnet ${name}`);
   }
 
-  public get natId(): string {
-    return this.nat.id;
-  }
+  // public get natId(): string {
+  //   return this.nat.id;
+  // }
 
-  public get snatId(): string {
-    return this.snat.id;
-  }
+  // public get snatId(): string {
+  //   return this.snat.id;
+  // }
 }
